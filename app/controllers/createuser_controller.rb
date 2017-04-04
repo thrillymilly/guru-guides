03752ecc84a8c3daa97754382f4a33c3928e01a6
William Miller <will.oliver.miller@gmail.com>
2017-04-04 11:15:02 +1000

    login/signup v1.1

--- app/controllers/createuser_controller.rb
+++ app/controllers/createuser_controller.rb
@@ -10,6 +10,20 @@ class CreateUserController < ApplicationController
 
     newUser.password = params[:password]
 
+    newUser.fullName = params[:full_name]
+
+    newUser.address1 = params[:address_1]
+
+    newUser.address2 = params[:address_2]
+
+    newUser.address1 = params[:address_1]
+
+    newUser.city = params[:city]
+
+    newUser.state = params[:state_code]
+
+    newUser.country = params[:country]
+
     if newUser.save
       session[:user_id] = newUser.id
       redirect '/'
