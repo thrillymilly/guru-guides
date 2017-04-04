eb70d4495c7b359bf9cbf5afb8c17c331033633b
William Miller <will.oliver.miller@gmail.com>
2017-04-04 16:32:47 +1000

    started work on landing page, added skeleton css

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  
+
   ##ROUTES FOR LOGIN##
-  get '/login', to: 'session#new'
   post '/session', to: 'session#create'
   delete '/session', to: 'session#destroy'
 
   ##ROUTES FOR NEW USER##
-  post '/createuser', to: 'session#create'
+  post '/createuser', to: 'user#create'
 
 end
