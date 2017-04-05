0c402be636320b914229b55beed95623b76cb371
Julian Chan <julian.chan@mail.com>
2017-04-05 16:49:43 +1000

    moved journey planner to partial; added location search functionality

--- app/assets/javascripts/plans.js
+++ app/assets/javascripts/plans.js
@@ -1,5 +1,36 @@
 $(function() {
+  var $suggestions = $('.suggestions');
+  var $search = $('.search');
+  var $selectedLocation = $('#selected-location');
+
+  $('.search').keyup(function() {
+    var input = $(this).val();
+
+    if (input.length === 0) {
+      $suggestions.empty();
+    } else if (input.length > 4) {
+      $.ajax({
+        url: '/api/locations/suggestions',
+        data: { input: $(this).val() }
+      }).done(function(results) {
+        $suggestions.empty();
+
+        results.forEach(function(result) {
+          $suggestions.append($('<li>').text(result.description).attr('data-id', result.place_id));
+        });
+      });
+    }
+  });
+
+  $suggestions.on('click', 'li', function() {
+    var location = $(this).text();
+
+    $search.val(location);
+    $selectedLocation.val(location);
+    $suggestions.empty();
+  });
+
   $('.plan').on('click', 'header', function() {
-    $(this).closest('div').find('.items').slideToggle(500);
+    $(this).closest('div').find('.items').slideToggle(300);
   });
 });
--- app/controllers/api/locations/locations_controller.rb
+++ app/controllers/api/locations/locations_controller.rb
@@ -4,4 +4,13 @@ class Api::Locations::LocationsController < Api::ApiController
 
     render json: get_address(latitude, longitude)
   end
+
+  def suggest
+    response = HTTParty.get("https://maps.googleapis.com/maps/api/place/autocomplete/json?input=#{params[:input]}&key=#{Rails.application.secrets.gmap_key}")
+
+    if response["status"] == "OK"
+      results = response["predictions"].map { |p| [p.assoc("description"), p.assoc("place_id")].to_h }
+      render json: results
+    end
+  end
 end
